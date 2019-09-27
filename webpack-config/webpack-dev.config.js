32a085ad5d8b1655a49b9a473e14bbbfa3a9281d
Wangyong <1139730843@qq.com>
2019-09-27 23:15:01 +0800

    add file

--- webpack-config/webpack-dev.config.js
+++ webpack-config/webpack-dev.config.js
@@ -5,6 +5,6 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve('./', 'dist')
+        path: path.resolve('./', 'dist') // 此目录是基于项目根目录来进行输出
     }
 };

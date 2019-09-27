93fbde8ecb1fe4c9d538f266654b66098485417c
Wangyong <1139730843@qq.com>
2019-09-27 21:58:47 +0800

    add file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ function component() {
     const ele = document.createElement('div');
     ele.innerHTML = 'hello, webpack';
     return ele;
+    console.log(ele);
 }
 
 document.body.appendChild(component());

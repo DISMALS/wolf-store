3fc4e7c2eabbeaa55afc5e629bbbbda28af8127e
Dismal <1139730843@qq.com>
2019-09-28 23:44:51 +0800

    add config

--- config/webpack-dev.config.js
+++ config/webpack-dev.config.js
@@ -4,6 +4,7 @@ const postCssPlugins = require('./postcss.plugins.js');
 
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, '../src/index.js'),
     output: {
         filename: 'main.js',
@@ -49,6 +50,41 @@ module.exports = {
                         loader: 'less-loader',
                         options: {
                            sourceMap: true 
+                        }
+                    }
+                ]
+            }, { // files
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            fallback: 'file-loader', // 引入file-loader
+                            limit: 1024, // 图片超过此大小则生成图片文件，否则使用编码嵌入代码中
+                            qulity: 80, // 图片质量
+                            outputPath: './assets/images/', // 输出路径
+                            name(file){ // 文件名
+                                if (process.env.NODE_ENV === 'production') {
+                                    return '[contenthsah:20]-[name].[ext]';
+                                }
+                                return '[hash:10]-[name].[ext]';
+                            }
+                        }
+                    }
+                ]
+            }, { // fonts 
+                test: /\.(ttf|eot|otf|woff)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outpurPath: './assets/fonts/',
+                            name(file) {
+                                if (process.env.NODE_ENV === 'production') {
+                                    return '[contenthash:20]-[name].[ext]';
+                                }
+                                return '[hash:10]-[name].[ext]';
+                            }
                         }
                     }
                 ]

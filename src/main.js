619d146b20d595876c82488d3bfd20965e41ac1f
Wangyong <1139730843@qq.com>
2019-10-17 11:22:46 +0800

    edit file content

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-if ('serviceWorker' in navigator) { // 离线缓存策略
-    window.addEventListener('load', () => {
-      // 敲黑板, 这里的service-wroker.js需要根据实际情况变化, 因为我项目没部署到根域名, 所以加了workbox的路径名...
-      // 注意: 这里有个坑 service-wroker 会被缓存, 解决方案在下面的坑点介绍
-      navigator.serviceWorker.register(`./service-wroker.js?${new Date().getTime()}`).then(registration => {
-        console.log('SW registered: ', registration);
-      }).catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      })
-    })
-}
-
+import serviceWorker from './serviceWorker';
 import './styles/index.css';
 import './styles/common.less';
 import { cloneDeep, clone } from 'lodash';
 import subFn from './components/sub';
+
+/**
+ * 启动本地缓存机制，实现离线访问
+ */
+serviceWorker();
+
+
+
 const component = () => {
     const ele = document.createElement('div');
     ele.innerHTML = 'hello, webpack';

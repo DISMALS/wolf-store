29ecbd3ac101f0542595089a88d08890fd7328b2
Dismal <1139730843@qq.com>
2019-10-20 09:52:01 +0800

    add file

--- src/components/home.tsx
+++
@@ -1,5 +0,0 @@
-import '../styles/color.less';
-function home() {
-    console.log('this is home');
-}
-home()
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import serviceWorker from './serviceWorker';
 import './index.less';
-import('./components/home').then(() => {
-    console.log('home is load');
-});
 /**
  * 启动本地缓存机制，实现离线访问
  */
